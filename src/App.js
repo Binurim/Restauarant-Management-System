5a7ef9b3e9098517f3689b09600d2164fe90487a
binuri <binurim95@gmail.com>
2020-03-22 23:25:43 +0530

    Components Part1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React , { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
+import Menu from './components/MenuComponent';
 import './App.css';
 
 
@@ -12,6 +13,7 @@ class App extends Component {
             <NavbarBrand href="/">Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu />
       </div>
     );
   }
