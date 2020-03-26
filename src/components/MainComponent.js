05e92555cc474105f5cbfc5c0410a73d2f14f3fd
binuri <binurim95@gmail.com>
2020-03-27 00:29:49 +0530

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,7 +4,11 @@ import Menu from './MenuComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import {DISHES} from '../shared/dishes';
+import {COMMENTS} from '../shared/comments';
+import {PROMOTIONS} from '../shared/promotions';
+import {LEADERS} from '../shared/leaders';
 import Home from './HomeComponent';
+import Contact from './ContactComponent';
 import { Switch, Route, Redirect } from 'react-router-dom';
 
 
@@ -15,6 +19,9 @@ class Main extends Component {
 
     this.state={
       dishes:DISHES,
+      comments:COMMENTS,
+      promotions:PROMOTIONS,
+      leaders:LEADERS
     //   selectedDish:null
     };
   }
@@ -28,7 +35,9 @@ class Main extends Component {
 
     const HomePage=()=>{
         return(
-            <Home/>
+            <Home dish={this.state.dishes.filter((dish)=>dish.featured)[0]}
+            promotion={this.state.promotions.filter((promo)=>promo.featured)[0]}
+            leader={this.state.leaders.filter((leader)=>leader.featured)[0]}/>
         );
     }
 
@@ -39,6 +48,7 @@ class Main extends Component {
         <Switch>
             <Route path="/home" component={HomePage}/>
             <Route exact path="/menu" component={()=><Menu dishes={this.state.dishes}/>}/>
+            <Route exact path='/contactus' component={Contact} />} />
             <Redirect to="/home"/>
         </Switch>
         {/* <Menu dishes={this.state.dishes} onClick={(dishId)=>this.onDishSelect(dishId)}/> */}
