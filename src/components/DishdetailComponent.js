8dff87f2520752292a96b5d351252eb1a13e5f68
binuri <binurim95@gmail.com>
2020-04-22 13:40:26 +0530

    Fetch from Server

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -2,16 +2,14 @@ import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle, Breadcrumb, BreadcrumbItem } from 'reactstrap';
 import {Link} from 'react-router-dom';
 import CommentForm from './CommentForm';
-import {Loading} from './LoadingComponent'
+import {Loading} from './LoadingComponent';
+import {baseUrl} from '../shared/baseUrl';
+
  function RenderDish({dish}) {
     return (
       <div>
         <Card>
-          <CardImg
-            width="100%"
-            src={dish.image}
-            alt={dish.name}
-          />
+          <CardImg width="100%" src={baseUrl + dish.image} alt={dish.name} />
           <CardBody>
             <CardTitle>{dish.name}</CardTitle>
             <CardText>{dish.description}</CardText>
