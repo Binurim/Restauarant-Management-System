8d8c47137545203c1a7dc3b48e3dd40afc186311
binuri <binurim95@gmail.com>
2023-04-30 18:20:10 +0530

    Updates

---
+++ precache-manifest.b4e2039d5d0cda13403422fe703f0ba1.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "feca2f6149f4c4d7f384c1da13dc5627",
+    "url": "/Restauarant-Management-System/index.html"
+  },
+  {
+    "revision": "89500dee8d8a69e88016",
+    "url": "/Restauarant-Management-System/static/css/2.bf6a1e66.chunk.css"
+  },
+  {
+    "revision": "284994aa4f9da701534a",
+    "url": "/Restauarant-Management-System/static/css/main.b3ef4b2c.chunk.css"
+  },
+  {
+    "revision": "89500dee8d8a69e88016",
+    "url": "/Restauarant-Management-System/static/js/2.0b18ff6e.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/Restauarant-Management-System/static/js/2.0b18ff6e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "284994aa4f9da701534a",
+    "url": "/Restauarant-Management-System/static/js/main.3fa7287c.chunk.js"
+  },
+  {
+    "revision": "bd6db3ca652967f94811",
+    "url": "/Restauarant-Management-System/static/js/runtime-main.671e1961.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Restauarant-Management-System/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Restauarant-Management-System/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Restauarant-Management-System/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Restauarant-Management-System/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Restauarant-Management-System/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
